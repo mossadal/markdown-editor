867e95319ce426765049e185d1a118e6ce7913b9
Frank Wikström <frank@mossadal.se>
2015-11-09 13:20:20 +0100

    Tweaking the preview-code

--- markdown-editor.js
+++ markdown-editor.js
@@ -6,62 +6,68 @@ $(window).load(function () {
     $('.md-button-bold').click(function() {
         s = $('textarea.md-preview-source').textrange();
 
-        // console.log('selection = ' + JSON.stringify(s));
+        console.log('selection = ' + JSON.stringify(s));
         if (s.length == 0) s.text = replace_text['bold'];
 
         replacement = '**' + s.text + '**';
         $('textarea.md-preview-source')
             .textrange('replace', replacement)
-            .textrange('set', s.start+2, replacement.length-4 );
+            .textrange('set', s.start+2, replacement.length-4 )
+            .trigger('input');
+
     });
 
     $('.md-button-italic').click(function() {
         s = $('textarea.md-preview-source').textrange();
 
-        // console.log('selection = ' + JSON.stringify(s));
+        console.log('selection = ' + JSON.stringify(s));
         if (s.length == 0) s.text = replace_text['italic'];
 
         replacement = '*' + s.text + '*';
         $('textarea.md-preview-source')
             .textrange('replace', replacement)
-            .textrange('set', s.start+1, replacement.length-2 );
+            .textrange('set', s.start+1, replacement.length-2 )
+            .trigger('input');
     });
 
     $('.md-button-blockquote').click(function() {
         s = $('textarea.md-preview-source').textrange();
 
-        // console.log('selection = ' + JSON.stringify(s));
+        console.log('selection = ' + JSON.stringify(s));
         if (s.length == 0) s.text = replace_text['quote'];
 
         replacement = '\n> ' + s.text;
         $('textarea.md-preview-source')
             .textrange('replace', replacement)
-            .textrange('set', s.start+3, replacement.length-3 );
+            .textrange('set', s.start+3, replacement.length-3 )
+            .trigger('input');
     });
 
     $('.md-button-url').click(function() {
         s = $('textarea.md-preview-source').textrange();
 
-        // console.log('selection = ' + JSON.stringify(s));
+        console.log('selection = ' + JSON.stringify(s));
         if (s.length == 0) s.text = replace_text['url'];
 
         replacement = '[' + replace_text['img_description'] + '](' + s.text + ')';
         $('textarea.md-preview-source')
             .textrange('replace', replacement)
-            .textrange('set', s.start+1, replace_text['img_description'].length );
+            .textrange('set', s.start+1, replace_text['img_description'].length )
+            .trigger('input');
 
     });
 
     $('.md-button-img').click(function() {
         s = $('textarea.md-preview-source').textrange();
 
-        // console.log('selection = ' + JSON.stringify(s));
+        console.log('selection = ' + JSON.stringify(s));
         if (s.length == 0) s.text = replace_text['url'];
 
         replacement = '![' + replace_text['img_description'] + '](' + s.text + ')';
         $('textarea.md-preview-source')
             .textrange('replace', replacement)
-            .textrange('set', s.start+2, replace_text['img_description'].length );
+            .textrange('set', s.start+2, replace_text['img_description'].length )
+            .trigger('input');
     });
 
     form_html = [
